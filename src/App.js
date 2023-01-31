f8a0424c1fbef3e0696691f1408a2c07957a522b
Hudson Romeu <hudson.romeu@codeinformatica.com.br>
2023-01-31 15:34:45 -0300

    Initial kit

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App() {
       <Content>
         <Container text textAlign='center'>
           <Image centered size='tiny' src={iconDevx} style={{marginBottom:"2em"}}/>
-          <p>Create React Ethereum DApp on Polygon</p>
-          <p>Build your first app with Starter Kits</p>
+          <p>It's my firt DApp on Polygon</p>
+          <p>I've created my DApp with Polygon - Starter Kits</p>
         </Container>
       </Content>
       <Footer/>
